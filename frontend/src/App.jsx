d87eafa4ddb5f4ead15b5de551b0fe7efe74047a
Kirti <kirti.air203@gmail.com>
2025-03-10 01:16:08 +0530

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,20 @@ const App = () => {
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
 
+  // function for button onclick 
+  const joinRoom = () =>{
+    if( roomId && userName ){
+      socket.emit("join", {roomId, userName});
+      setJoined(true);
+    }
+  };
+
+  // function for copy Room Id
+
+  const copyRoomId = () =>{
+
+  };
+
   if( !joined ){
     return (
       <div className="join-container">
@@ -31,13 +45,29 @@ const App = () => {
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
           />
-          <button > Join Room </button>
+          <button onClick={joinRoom}> Join Room </button>
 
         </div>
       </div>
     );
   }
-  return <div>User joined</div>;
+  return (
+    <div className="edito-container">
+      <div className="sidebar">
+        <div className="room-info">
+          <h2>Code Room :{roomId}</h2>
+          <button onClick={copyRoomId}>Copy Id</button>
+        </div>
+        <h3>User in Room:</h3>
+        <ul>
+          <li> Ram</li>
+          <li> Krishna</li>
+        </ul>
+        <p className="typing-indicator"> User typing... </p>
+
+      </div>
+    </div>
+  )
 };
 
 export default App;
