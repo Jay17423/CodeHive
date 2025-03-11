1bf1fc0742ddb1b6bad4efe4b14cbeb83d9b31c1
Kirti <kirti.air203@gmail.com>
2025-03-12 04:18:09 +0530

    leave room functionlity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import io from "socket.io-client";
-import Editor from '@monaco-editor/react'
-
+import Editor from "@monaco-editor/react";
 
 const socket = io("http://localhost:5050");
 
 const App = () => {
-
   const [joined, setJoined] = useState(false);
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
-  const [language, setLanguage] = useState("javascript");  // default language as JavaScript
-  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("javascript"); // default language as JavaScript
+  const [code, setCode] = useState("// start code here ");
   const [copySuccess, setCopySuccess] = useState("");
   const [users, setUsers] = useState([]);
   const [typing, setTyping] = useState([]);
-  
-  useEffect( () => {
+
+  useEffect(() => {
     socket.on("userJoined", (users) => {
       setUsers(users);
     });
@@ -25,18 +23,18 @@ const App = () => {
     // code update functionality
     socket.on("codeUpdate", (newCode) => {
       setCode(newCode);
-    }) 
+    });
 
     // typing indicator functionality
-    socket.on("userTyping", (user)=>{
-      setTyping(`${user.slice(0, 8)}... is Typing`)
-      setTimeout(()=> setTyping(""), 6000); // empty it after 2 seconds
+    socket.on("userTyping", (user) => {
+      setTyping(`${user.slice(0, 8)}... is Typing`);
+      setTimeout(() => setTyping(""), 6000); // empty it after 2 seconds
     });
-  
+
     // for language change
     socket.on("languageUpdate", (newLanguage) => {
       setLanguage(newLanguage);
-    })
+    });
 
     // cleanup function for socket off
     return () => {
@@ -49,7 +47,7 @@ const App = () => {
 
   // useEffect tp handle room on reload the page
 
-  useEffect( ()=> {
+  useEffect(() => {
     const handleBeforeUnload = () => {
       socket.emit("leaveRoom");
     };
@@ -61,38 +59,46 @@ const App = () => {
     };
   }, []);
 
-  // function for button onclick 
-  const joinRoom = () =>{
-    if( roomId && userName ){
-      socket.emit("join", {roomId, userName});
+  /*   function for button onclick   */
+  const joinRoom = () => {
+    if (roomId && userName) {
+      socket.emit("join", { roomId, userName });
       setJoined(true);
     }
   };
 
+  /*   function for leave room    */
+  const leaveRoom = () => {
+    socket.emit("leaveRoom");
+    setJoined("false");
+    setRoomId("");
+    setUserName("");
+    setCode("// start code here");
+    setLanguage("javascript");
+  };
 
-  // function for copy Room Id
-  const copyRoomId = () =>{
+  /* function for copy Room Id  */
+  const copyRoomId = () => {
     navigator.clipboard.writeText(roomId);
     setCopySuccess("Copied!");
     setTimeout(() => setCopySuccess(""), 2000);
   };
 
-
   // function to handle the edited code on the code editor
-  const handleCodeChange = (newCode) =>{
+  const handleCodeChange = (newCode) => {
     setCode(newCode);
-    socket.emit("codeChange", {roomId, code: newCode});
-    socket.emit("typing", {roomId, userName});  // typing indicator
-  }
+    socket.emit("codeChange", { roomId, code: newCode });
+    socket.emit("typing", { roomId, userName }); // typing indicator
+  };
 
   // function to handle the language change
   const handleLanguageChange = (e) => {
     const newLanguage = e.target.value;
     setLanguage(newLanguage);
-    socket.emit("languageChange", {roomId, language: newLanguage});
-  }
+    socket.emit("languageChange", { roomId, language: newLanguage });
+  };
 
-  if( !joined ){
+  if (!joined) {
     return (
       <div className="join-container">
         <div className="join-form">
@@ -114,7 +120,6 @@ const App = () => {
             onChange={(e) => setUserName(e.target.value)}
           />
           <button onClick={joinRoom}> Join Room </button>
-
         </div>
       </div>
     );
@@ -124,46 +129,46 @@ const App = () => {
     <div className="editor-container">
       <div className="sidebar">
         <div className="room-info">
-          <h2> Code Room :{roomId} </h2>
-          <button className="copy-button" onClick={copyRoomId}> Copy Id </button>
+          <h2> Code Room : {roomId} </h2>
+          <button className="copy-button" onClick={copyRoomId}>Copy Id</button>
           {/* if copied then show it  */}
-          { copySuccess && <span className="copy-success">{copySuccess}</span>}
+          {copySuccess && <span className="copy-success">{copySuccess}</span>}
         </div>
         <h3>Users in Room</h3>
         <ul>
-          {
-            users.map((user, index) => (
-              <li key={index}> {user.slice(0,8)}..</li>
-            ))
-          }
+          {users.map((user, index) => (
+            <li key={index}> {user.slice(0, 8)}..</li>
+          ))}
         </ul>
         <p className="typing-indicator"> {typing} </p>
-        { /* to choose the language of our choice */}
-        <select 
-          className="language-selector" 
+        {/* to choose the language of our choice */}
+        <select
+          className="language-selector"
           value={language}
           onChange={handleLanguageChange}
-          >
+        >
           <option value="javascript">JavaScript</option>
           <option value="cpp">C++</option>
           <option value="c">C</option>
           <option value="python">Python</option>
           <option value="java">Java</option>
         </select>
-        <button className="leave-button">Leave Room</button>
+        <button className="leave-button" onClick={leaveRoom}>
+          Leave Room
+        </button>
       </div>
       <div className="editor-wrapper">
-        <Editor 
-          height= {"100%"}
-          defaultLanguage= {language}
-          language= {language}
-          value= {code}
-          onChange= {handleCodeChange}
-          theme= "vs-dark"
-          options ={{
-              minimap: { enabled: false },
-              fontSize: 14,
-            }}
+        <Editor
+          height={"100%"}
+          defaultLanguage={language}
+          language={language}
+          value={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+          }}
         />
       </div>
     </div>
