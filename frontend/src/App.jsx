65700727fe53c8289a2e3d8336a06bb6e4f742eb
Jay Prakash <jayprakash1200@gmail.com>
2025-03-20 02:39:07 +0530

    Fix the refresh issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-// App.js
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
-import { setCode, setLanguage, setRoomId, setVersion } from "./Slice/CodeSlice";
+import { setCode, setLanguage, setRoomId } from "./Slice/CodeSlice";
 import JoinRoom from "./Components/JoinRoom";
 import Sidebar from "./Components/Sidebar";
 import CodeEditor from "./Components/CodeEditor";
@@ -13,19 +12,13 @@ const socket = io("http://localhost:5050");
 
 const App = () => {
   const dispatch = useDispatch();
-  const { code, language, roomId, version } = useSelector(
-    (state) => state.code
-  );
+  const { code, language, roomId, version } = useSelector((state) => state.code);
   const [joined, setJoined] = useState(false);
   const [userName, setUserName] = useState("");
   const [copySuccess, setCopySuccess] = useState("");
   const [users, setUsers] = useState([]);
   const [typing, setTyping] = useState("");
   const [output, setOutput] = useState("");
-
-
-
-
 
   useEffect(() => {
     const handleBeforeUnload = (event) => {
@@ -34,20 +27,32 @@ const App = () => {
     };
 
     const handlePopState = () => {
-      const confirmNavigation = window.confirm(
-        "Are you sure you want to leave this page?"
-      );
+      const confirmNavigation = window.confirm("Are you sure you want to leave this page?");
       if (!confirmNavigation) {
         window.history.pushState(null, "", window.location.href);
+      } else {
+        socket.emit("leaveRoom");
+      }
+    };
+
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "hidden") {
+        setTimeout(() => {
+          if (document.visibilityState === "hidden") {
+            socket.emit("leaveRoom");
+          }
+        }, 1000);
       }
     };
 
     window.addEventListener("beforeunload", handleBeforeUnload);
     window.addEventListener("popstate", handlePopState);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
       window.removeEventListener("popstate", handlePopState);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
   }, []);
 
@@ -73,23 +78,12 @@ const App = () => {
       setOutput(response.run.output);
     });
 
-
     return () => {
       socket.off("userJoined");
       socket.off("codeUpdate");
       socket.off("userTyping");
       socket.off("languageUpdate");
       socket.off("codeResponse");
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      socket.emit("leaveRoom");
-    };
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
 
@@ -133,13 +127,9 @@ const App = () => {
     socket.emit("compileCode", { code, language, roomId, version });
   };
 
-
-
   const downloadCode = () => {
     const hardcodedData = `// Room ID: ${roomId}\n // User Name: ${userName}\n`;
-
     const finalCode = hardcodedData + code;
-
     const blob = new Blob([finalCode], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
@@ -151,9 +141,7 @@ const App = () => {
       cpp: "cpp",
       java: "java",
     };
-
     a.download = `code.${extensions[language] || "txt"}`;
-
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -174,39 +162,26 @@ const App = () => {
 
   return (
     <div className="editor-container">
-      
-        <Sidebar
-          roomId={roomId}
-          users={users}
-          typing={typing}
-          copyRoomId={copyRoomId}
-          copySuccess={copySuccess}
-          leaveRoom={leaveRoom}
-          downloadCode={downloadCode}
-          language={language}
-          handleLanguageChange={handleLanguageChange}
-        />
+      <Sidebar
+        roomId={roomId}
+        users={users}
+        typing={typing}
+        copyRoomId={copyRoomId}
+        copySuccess={copySuccess}
+        leaveRoom={leaveRoom}
+        downloadCode={downloadCode}
+        language={language}
+        handleLanguageChange={handleLanguageChange}
+      />
       <div className="editor-wrapper">
-        <div className="editor-header">
-          
-        </div>
-        <CodeEditor
-          language={language}
-          code={code}
-          handleCodeChange={handleCodeChange}
-        />
+        <CodeEditor language={language} code={code} handleCodeChange={handleCodeChange} />
         <div className="run-container">
-          <button className="run-btn" onClick={runCode}>
-            Execute
-          </button>
+          <button className="run-btn" onClick={runCode}>Execute</button>
           <OutputConsole output={output} />
         </div>
       </div>
-      
-
     </div>
   );
 };
-
 
 export default App;
